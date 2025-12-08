d1a3662f7e63383eea19df3154bb54d5f81eec11
omar <omarqais225@gmail.com>
2025-12-08 23:53:06 +0200

    add voice animation

--- client/src/pages/AiPages/AiCallPage.tsx
+++ client/src/pages/AiPages/AiCallPage.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import LinkAi from "../../ui/links/LinkAi";
-import NavigateBack from "../../ui/navigators/NavigateBack";
+import Animation from "../../utils/Animation";
+import blueAnimationRecord from '../../animation/BlueAudio.json';
+import { StopCircle } from "lucide-react";
 const AiCallPage = () => {
+    const [isRecording, setIsRecording] = useState(false);
     return (
         <div className="w-full min-h-[85vh] flex flex-col items-center justify-center gap-5">
             <h1 className="mt-3">سدد فاتورتك بسهولة من خلال التحدث الى</h1>
@@ -9,10 +13,32 @@ const AiCallPage = () => {
             <div className="w-full flex gap-5 flex-col items-center text-center justify-center">
                 <p className="text-[#7D7E83] text-xs leading-5">اريد تسديد مخالفة المرور الخاص في محمد رزق ابو علي مخالفة رقم 456789 عن طريق تطبيق مركبتي</p>
                 <div className="w-full max-w-md flex items-center justify-between p-3">
-                    <LinkAi image="/icons/message.png" direction="/chat" />
-                    <button type="button" className="flex mb-14 items-center justify-center">
-                        <img src="/icons/microphone-call.png" alt="" />
-                    </button>
+                    {
+                        isRecording ?
+                            <div>
+                                <button onClick={() => {
+                                    setIsRecording(!isRecording)
+                                }} type="button" className="cursor-pointer active:opacity-50 active:scale-75 duration-300 ease transition-all w-12 h-12 flex items-center justify-center bg-red-400 rounded-full">
+                                    <StopCircle color="white" />
+                                </button>
+                            </div>
+                            : <LinkAi image="/icons/message.png" direction="/chat" />
+                    }
+                    {
+                        isRecording ?
+                            <div className="mb-14">
+                                <Animation animation={blueAnimationRecord} width={400} height={400} />
+                            </div>
+
+                            :
+                            <button
+                                onClick={() => {
+                                    setIsRecording(!isRecording)
+                                }}
+                                type="button" className="cursor-pointer active:scale-75 duration-300 ease transition-all flex mb-14 items-center justify-center">
+                                <img src="/icons/microphone-call.png" alt="" />
+                            </button>
+                    }
                     <LinkAi image="/icons/x.png" direction="/dashboard/ai" />
                 </div>
             </div>
