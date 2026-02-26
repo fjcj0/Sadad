5c79b4822eb84d82a5a7e9da4999820a84ac2266
omar <omarqais225@gmail.com>
2026-02-26 23:00:40 +0200

    use gorq in production

--- server/src/utils/AskAi.js
+++ server/src/utils/AskAi.js
@@ -1,27 +1,37 @@
 import axios from "axios";
+import Groq from "groq-sdk";
+const groq = new Groq({
+  apiKey: process.env.API_KEY, 
+});
 export const AskAi = async (messages) => {
   try {
+    if (process.env.NODE_ENV === "production") {
+      const completion = await groq.chat.completions.create({
+        model: "openai/gpt-oss-20b",
+        messages,
+      });
+      return completion.choices[0]?.message?.content || "";
+    }
     const response = await axios.post(
       "http://127.0.0.1:11434/api/chat",
       {
         model: "qwen2.5:14b",
         messages,
         stream: false,
-        format: "json",
         options: {
           temperature: 0.2,
           num_predict: 250,
-          top_k: 30
-        }
+          top_k: 30,
+        },
       },
       {
         headers: { "Content-Type": "application/json" },
-        timeout: 60000
+        timeout: 60000,
       }
     );
     return response.data.message.content;
   } catch (error) {
-    console.error("Ollama Error:", error.response?.data || error.message);
+    console.error("AI Error:", error.response?.data || error.message);
     throw new Error(error.message || "AI request failed");
   }
 };
